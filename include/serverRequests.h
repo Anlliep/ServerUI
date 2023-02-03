4ae28c5f58db47e9d8a8682972e418ccaa9f6c5c
Nazar Drozd <nazardrozd228@gmail.com>
2023-02-04 00:27:54 +0200

    fix icons

---
+++ include/serverRequests.h
@@ -0,0 +1,16 @@
+﻿#pragma once
+#include <iostream>
+#include <vector>
+#include "Poco/Net/HTTPClientSession.h"
+#include "Poco/Net/HTTPMessage.h"
+#include "Poco/Net/HTTPRequest.h"
+#include "Poco/Net/HTTPResponse.h"
+#include "Poco/StreamCopier.h"
+#include "Poco/URI.h"
+
+using namespace Poco;
+using namespace Poco::Net;
+
+namespace connection {
+std::vector<std::string> GetLogs();
+}
---
+++ src/serverRequests.cpp
@@ -0,0 +1,30 @@
+﻿#pragma once
+#include "serverRequests.h"
+
+std::vector<std::string> connection::GetLogs() {
+  URI uri("http://15.188.57.7:8080");
+  HTTPClientSession session(uri.getHost(), uri.getPort());
+  std::string path = "/logs";
+
+  session.setKeepAliveTimeout(true);
+
+  HTTPRequest request(HTTPRequest::HTTP_GET, path, HTTPMessage::HTTP_1_1);
+
+  session.sendRequest(request);
+
+  HTTPResponse response;
+
+  std::istream& page = session.receiveResponse(response);
+
+  std::vector<std::string> logs;
+
+  std::string line{};
+  std::string str{};
+  while (std::getline(page, line)) {
+    std::stringstream strStream(line);
+    while (std::getline(strStream, str, ',')) {
+      logs.push_back(str);
+    }
+  }
+  return logs;
+}
